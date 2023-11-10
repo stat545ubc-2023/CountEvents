7d0238ff1b2a7d4613fc1c8e30e3b3334af10244
Juliana Vieira <juliana.mfvieira@gmail.com>
2023-11-09 22:10:25 -0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# CountEvents
+
+The goal of `CountEvents` is to take a data frame or Tibble, and two
+variable names as argument.
+
+It performs data validation, filtering out rows with missing values.
+Then, counts the number of events in the data, grouped by the two
+specified variables, and returns the result as a data frame
+
+## Installation
+
+You can install the development version of `CountEvents` from
+[GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("stat545ubc-2023/CountEvents")
+```
+
+## Example
+
+This is a basic example which shows you how to use `CountEvents`:
+
+``` r
+library(CountEvents)
+library(palmerpenguins)
+suppressPackageStartupMessages(library(dplyr))
+#> Warning: package 'dplyr' was built under R version 4.3.2
+
+
+### *Using the penguins data*
+
+# How many penguins of each species per island in the data??
+specie_by_island <- count_events(penguins, species, island)
+print(specie_by_island)
+#> # A tibble: 5 × 3
+#>   species   island    count
+#>   <fct>     <fct>     <int>
+#> 1 Adelie    Biscoe       44
+#> 2 Adelie    Dream        55
+#> 3 Adelie    Torgersen    47
+#> 4 Chinstrap Dream        68
+#> 5 Gentoo    Biscoe      119
+```
---
+++ tests/testthat/test-count_events.R
@@ -0,0 +1,64 @@
+# ***Test 1.*** I will check if my function only accepts data frame or Tibble
+
+test_that("count_events only accepts data frame or Tibble", {
+
+  #make a vector to use as a test
+  n_vector_test <- c(1, 2, 3, 4, 5)
+  #make a variable to use as a test
+  var_test <- "not a dataset"
+
+
+  expect_error(count_events(n_vector_test, species, island), "Input 'data' must be a data frame or Tibble.") # A vector was used instead of a data frame or tibble, thus an error is expected
+  expect_error(count_events(var_test, species, island), "Input 'data' must be a data frame or Tibble.") # A single variable was used instead of a data frame or tibble, thus an error is expected
+})
+
+
+# ***Test 2.*** I will check if my function handles invalid inputs for var1 and var2
+
+test_that("count_events function handles invalid input", {
+
+  #make a variable not present in the dataset
+  var3 <- c(1, 2, 3, 4, 5)
+
+  #using penguins as dataset
+  library(palmerpenguins)
+
+  expect_error(count_events(palmerpenguins::penguins, var3, island), "Variables 'var1' must exist in the input data.") # var3 is not in the dataset, thus an error is expected for var1
+  expect_error(count_events(palmerpenguins::penguins, species, var3), "Variables 'var2' must exist in the input data.") # var3 is not in the dataset, thus an error is expected for var2
+})
+
+# ***Test 3.*** I will check if the function correctly counts events by 'var1' and 'var2'
+
+test_that("count_events function correctly counts events", {
+
+  #load necessary library
+  library(dplyr)
+
+  # Make a sample dataset
+  data1 <- data.frame(var1 = c("A", "A", "B", "B", "A", "B"),
+                     var2 = c("X", "Y", "X", "X", "Y", "Y"))
+
+  # Call the count_events function
+  result1 <- count_events(data1, var1, var2)
+
+  # Check if the result contains the expected counts
+  expect_equal(result1$count, c(1, 2, 2, 1))  #expect X:A = 1; X:B = 2; expect Y:A = 2; Y:B = 1;
+})
+
+# ***Test 4.*** I will check if the function handles NA values correctly
+
+test_that("count_events function NA test", {
+
+  #load necessary library
+  library(dplyr)
+
+  # Make a sample dataset
+  data2 <- data.frame(var1 = c(NA, "A", "B", "B", "A", "B"), #added an NA in the place of "A"
+                     var2 = c("X", "Y", "X", "X", NA, "Y")) #added an NA in the place of "Y"
+
+  # Call the count_events function
+  result2 <- count_events(data2, var1, var2)
+
+  # Check if the result contains the expected counts
+  expect_equal(result2$count, c(1, 2, 1))  #expect minus one row and minus one count for "Y"
+})
